32d665163a75add86c2bb6df7dc98483d28d8ba9
Juan Rodriguez <juan.j.rodriguez2@gmail.com>
2016-11-29 21:18:04 -0600

    sending and retrieving data from firebase

--- data/controllers/blog.controller.js
+++ data/controllers/blog.controller.js
@@ -1,18 +1,36 @@
-function BlogController(firebase){
-	
+function BlogController($scope,firebase, $timeout, $firebaseObject){
+	$scope.blog=[];
 
+	// function blogData2() {
+	// 	console.log("hello from blogData2 function");
+
+	// 	var dbRefObject = firebase.database().ref('/blog-posts/').orderByKey();
+	// 	this.object = $firebaseObject(dbRefObject);
+	// 	console.log("this.object:",this.object);
+	// 	dbRefObject.on('value',function(snap){
+	// 		console.log(snap.val());
+	// 	});
+	
+	
+		
+	// }
 	function blogData(){ 
+		console.log("hello from blogData function");
+
 		var query = firebase.database().ref("/blog-posts/").orderByKey();
 			query.once("value").then(function(snapshot) {
-				this.blog = [];
+				$scope.blog = [];
 			    snapshot.forEach(function(childSnapshot) {
 			     	var dataFromBlog = childSnapshot.val();
-			     	 this.blog.push(dataFromBlog);
+			     		console.log("dataFromBlog:",dataFromBlog);
+			     	 $scope.blog.push(dataFromBlog);
 			  	});
-			  	return this.blog;	
+			    console.log("$scope.blog:",$scope.blog);
+			  	 return $scope.blog;	
 			});
 	}
-	this.blog =[{
+	this.blog =[
+	{
 		title: 'First Post',
 		date: "November 29, 2016",
 		post: "Pork belly succulents polaroid helvetica, paleo street art bicycle rights mixtape iceland tousled. Listicle four dollar toast chillwave drinking vinegar kale chips. Semiotics art party small batch selfies, succulents microdosing vexillologist. Meh aesthetic brunch williamsburg. Raw denim pour-over knausgaard, tumblr shabby chic disrupt letterpress fashion axe XOXO distillery tacos enamel pin coloring book. Kombucha +1 lomo, enamel pin scenester gluten-free cred brunch deep v. Polaroid tbh try-hard drinking vinegar."
@@ -21,18 +39,21 @@ function BlogController(firebase){
 		title: 'Second Post',
 		date: "November 29, 2016",
 		post: "Pork belly succulents polaroid helvetica, paleo street art bicycle rights mixtape iceland tousled. Listicle four dollar toast chillwave drinking vinegar kale chips. Semiotics art party small batch selfies, succulents microdosing vexillologist. Meh aesthetic brunch williamsburg. Raw denim pour-over knausgaard, tumblr shabby chic disrupt letterpress fashion axe XOXO distillery tacos enamel pin coloring book. Kombucha +1 lomo, enamel pin scenester gluten-free cred brunch deep v. Polaroid tbh try-hard drinking vinegar."
-	},{
+	},
+	{
 		title: 'Third Post',
 		date: "November 29, 2016",
 		post: "Pork belly succulents polaroid helvetica, paleo street art bicycle rights mixtape iceland tousled. Listicle four dollar toast chillwave drinking vinegar kale chips. Semiotics art party small batch selfies, succulents microdosing vexillologist. Meh aesthetic brunch williamsburg. Raw denim pour-over knausgaard, tumblr shabby chic disrupt letterpress fashion axe XOXO distillery tacos enamel pin coloring book. Kombucha +1 lomo, enamel pin scenester gluten-free cred brunch deep v. Polaroid tbh try-hard drinking vinegar."
-	},{
+	},
+	{
 		title: 'Fourth Post',
 		date: "November 29, 2016",
 		post: "Pork belly succulents polaroid helvetica, paleo street art bicycle rights mixtape iceland tousled. Listicle four dollar toast chillwave drinking vinegar kale chips. Semiotics art party small batch selfies, succulents microdosing vexillologist. Meh aesthetic brunch williamsburg. Raw denim pour-over knausgaard, tumblr shabby chic disrupt letterpress fashion axe XOXO distillery tacos enamel pin coloring book. Kombucha +1 lomo, enamel pin scenester gluten-free cred brunch deep v. Polaroid tbh try-hard drinking vinegar."
-	}];
-blogData();
+	}
+	];
+// blogData();
 }
-BlogController.$inject=['firebase'];
+BlogController.$inject=['$scope','firebase', '$timeout', "$firebaseObject"];
 angular
 	.module('app')
 	.controller('BlogController', BlogController);
