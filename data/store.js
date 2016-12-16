d7e0559a3f37b7f8432a0ccef69bc2b207cf84c4
Juan Rodriguez <juan.j.rodriguez2@gmail.com>
2016-12-16 16:07:48 -0600

    changed blog font and sizing of blog post

--- data/store.js
+++ data/store.js
@@ -5,6 +5,11 @@
 	    new product("AVC", "The Eva Longoria", "Something about her...", 16, 90, 0, 1, 1, 1, 2),
 	    new product("BAN", "The Eva Marie Saint", "Holy Holy", 4, 120, 0, 2, 1, 2, 2),
 	    // more products…
+	    new product("WML", "The Eva Peron", "Perdon, Peron", 4, 90, 4, 4, 0, 1, 1),
+	    new product("APL", "The Eva Gabor", "Money can't buy you class", 12, 90, 0, 2, 0, 1, 2),
+	    new product("AVC", "The Eva Longoria", "Something about her...", 16, 90, 0, 1, 1, 1, 2),
+	    new product("BAN", "The Eva Marie Saint", "Holy Holy", 4, 120, 0, 2, 1, 2, 2),
+	    // more products…
 	    new product("WML", "The Eva Peron", "Perdon, Peron", 4, 90, 4, 4, 0, 1, 1)
 	  ];
 	  this.dvaCaption = ["Negligible", "Low", "Average", "Good", "Great" ];
